ac69eb73fd401c37f0d0665f505391c1bc92e828
Adam Kittelson <adam@zencoder.com>
2011-07-27 11:03:06 -0700

    use .future? since we have activerecord

--- lib/recurrent/task.rb
+++ lib/recurrent/task.rb
@@ -9,7 +9,7 @@ module Recurrent
     end
 
     def next_occurrence
-      return @next_occurrence if @next_occurrence && (@next_occurrence > Time.now)
+      return @next_occurrence if @next_occurrence && @next_occurrence.future?
       @next_occurrence = schedule.next_occurrence
     end
 
