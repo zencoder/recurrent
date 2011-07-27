83cff29799734e744d8bcff7e7fc5c2568740ea0
Adam Kittelson <adam@zencoder.com>
2011-07-27 14:24:24 -0700

    spec next_task_time

--- spec/scheduler_spec.rb
+++ spec/scheduler_spec.rb
@@ -2,8 +2,22 @@ require 'spec_helper'
 
 module Recurrent
   describe Scheduler do
-    it "should pass" do
-      1.should == 1
+    describe "#next_task_time" do
+      context "when there are multiple tasks" do
+        it "should return the soonest time at which a task is scheduled" do
+          task1 = stub('task')
+          task1.stub(:next_occurrence).and_return(10.minutes.from_now)
+          task2 = stub('task')
+          task2.stub(:next_occurrence).and_return(5.minutes.from_now)
+          task3 = stub('task')
+          task3.stub(:next_occurrence).and_return(15.minutes.from_now)
+          schedule = Scheduler.new
+          schedule.tasks << task1
+          schedule.tasks << task2
+          schedule.tasks << task3
+          schedule.next_task_time.should == task2.next_occurrence
+        end
+      end
     end
   end
 end
