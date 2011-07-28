8495ead886fc591ab8abda4e4fb491460bf37ae3
Adam Kittelson <adam@zencoder.com>
2011-07-27 17:14:14 -0700

    fix nesting

--- lib/recurrent/configuration.rb
+++ lib/recurrent/configuration.rb
@@ -3,23 +3,23 @@ module Recurrent
 
     class << self
       attr_accessor :task_file
-    end
 
-    def self.block_accessor(*fields)
-      fields.each do |field|
-        attr_writer field
-        eval("
-        def #{field}
-          if block_given?
-            @#{field} = Proc.new
-          else
-            @#{field}
+      def self.block_accessor(*fields)
+        fields.each do |field|
+          attr_writer field
+          eval("
+          def #{field}
+            if block_given?
+              @#{field} = Proc.new
+            else
+              @#{field}
+            end
           end
+          ")
         end
-        ")
       end
+      block_accessor :logger
+    
     end
-    block_accessor :logger
-
   end
 end
