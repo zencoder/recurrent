cbed97149113644a3e8fbcf5bbc61ad51936f84a
Adam Kittelson <adam@zencoder.com>
2011-07-28 12:12:18 -0700

    fix rake task arguments, slim down task to rake recurrent:run_tasks[optional path to task file]

--- lib/recurrent/rake_tasks.rb
+++ lib/recurrent/rake_tasks.rb
@@ -1,9 +1,7 @@
 namespace :recurrent do
-  namespace :scheduler do
-    desc "Run the scheduler."
-    task :execute_tasks => :environment do |tasks_file|
-      tasks_file ||= "#{Rails.root}/config/recurrences.rb"
-      Recurrent::Scheduler.new(tasks_file).execute
-    end
+  desc "Run the scheduler."
+  task :run_tasks, [:task_file] => :environment do |t, args|
+    tasks_file = args[:task_file] || "#{Rails.root}/config/recurrences.rb"
+    Recurrent::Scheduler.new(tasks_file).execute
   end
 end
