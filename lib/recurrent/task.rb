16fb22bbedf12537e2ea9e53349973ef9e594d24
Adam Kittelson <adam@zencoder.com>
2011-08-05 13:02:40 -0700

    killed a blank line

--- lib/recurrent/task.rb
+++ lib/recurrent/task.rb
@@ -38,7 +38,6 @@ module Recurrent
       end
     end
 
-
     def next_occurrence
       return @next_occurrence if @next_occurrence && @next_occurrence.future?
       @next_occurrence = schedule.next_occurrence
