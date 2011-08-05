410f8c69975eca6a3c19ad270201195190b5a6dd
Adam Kittelson <adam@zencoder.com>
2011-08-05 15:47:05 -0700

    spec #wait_until

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -2,5 +2,16 @@ require 'spec_helper'
 
 module Recurrent
   describe Worker do
+    describe "#wait_until" do
+      it "waits until a specified time" do
+        Timecop.freeze(Time.local(2011, 7, 26, 11, 35, 00))
+        waiting_thread = Thread.new { Worker.new.wait_until(Time.local(2011, 7, 26, 11, 40, 00)) }
+        waiting_thread.alive?.should be_true
+        Timecop.travel(Time.local(2011, 7, 26, 11, 40, 00))
+        sleep(0.5)
+        waiting_thread.alive?.should be_false
+        Timecop.return
+      end
+    end
   end
 end
