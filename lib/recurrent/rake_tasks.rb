bf2881a0f94cef5729285d64054aff521af0bc3a
Adam Kittelson <adam@zencoder.com>
2011-08-04 17:18:01 -0700

    remove rake task

--- lib/recurrent/rake_tasks.rb
+++
@@ -1,7 +0,0 @@
-namespace :recurrent do
-  desc "Run the scheduler."
-  task :run_tasks, [:task_file] => :environment do |t, args|
-    tasks_file = args[:task_file] || "#{Rails.root}/config/recurrences.rb"
-    Recurrent::Worker.new(tasks_file).start
-  end
-end
