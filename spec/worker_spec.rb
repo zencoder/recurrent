1ab39fc3d2e5f2a7e2e1b957473f5de86a9fecee
Adam Kittelson <adam@zencoder.com>
2011-08-05 17:25:13 -0700

    spec wait_for_running_tasks_indefinitely

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -2,6 +2,34 @@ require 'spec_helper'
 
 module Recurrent
   describe Worker do
+    describe "#wait_for_running_tasks_indefinitely" do
+      context "A worker with no running tasks" do
+        before :each do
+          @worker = Worker.new
+        end
+
+        it "logs that all tasks are finished and returns true" do
+          @worker.logger.should_receive(:info).with("All tasks finished, exiting...")
+          @worker.wait_for_running_tasks_indefinitely.should be_true
+        end
+      end
+
+      context "A worker with running tasks" do
+        before :each do
+          @worker = Worker.new
+          @task = Task.new :name => 'worker_test'
+          @task.thread = Thread.new { sleep 0.1 }
+          @worker.scheduler.tasks << @task
+        end
+
+        it "waits for the task to finish and returns true" do
+          @worker.logger.should_receive(:info).with("Waiting for worker_test to finish.")
+          @worker.logger.should_receive(:info).with("All tasks finished, exiting...")
+          @worker.wait_for_running_tasks_indefinitely.should be_true
+        end
+      end
+    end
+
     describe "#wait_until" do
       it "waits until a specified time" do
         Timecop.freeze(Time.local(2011, 7, 26, 11, 35, 00))
