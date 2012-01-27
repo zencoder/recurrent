34e377cb80bf8c81b20dc8a28839adf554624d3d
Adam Kittelson <adam@zencoder.com>
2012-01-26 17:19:08 -0800

    put the sort_by_frequency stuff in the right place

--- lib/recurrent/worker.rb
+++ lib/recurrent/worker.rb
@@ -41,7 +41,7 @@ module Recurrent
     def execute
       loop do
         execution_time = scheduler.next_task_time
-        tasks_to_execute = scheduler.tasks_at_time(execution_time)
+        tasks_to_execute = scheduler.tasks.scheduled_to_execute_at(execution_time, :sort_by_frequency => !!Configuration.maximum_concurrent_tasks)
 
         wait_for_running_tasks && break if $exit
 
@@ -49,7 +49,7 @@ module Recurrent
 
         wait_for_running_tasks && break if $exit
 
-        tasks_to_execute(:sort_by_frequency => !!Configuration.maximum_concurrent_tasks).each do |task|
+        tasks_to_execute.each do |task|
           logger.info "#{task.name}: Executing at #{execution_time.to_s(:seconds)}"
           task.execute(execution_time)
         end
